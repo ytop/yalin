1bbd319d846417ec19da4c2af59dc9c7d2758cf3
ylhca <ylhca@DESKTOP-V8RKU25>
2020-06-30 23:29:31 -0400

    beautify v2

--- comment.php
+++ comment.php
@@ -4,20 +4,20 @@ include_once("connect.php");
 //htmlspecialchars() 函数把预定义的字符转换为 HTML 实体
 $user = htmlspecialchars(trim($_POST['user']));
 $txt = htmlspecialchars(trim($_POST['txt']));
-if(empty($user)){
-    $data = array("code"=>355,"message"=>"名字不能为空！");
+if (empty($user)) {
+    $data = array("code" => 355, "message" => "名字不能为空！");
     echo json_encode($data);
     exit;
 }
-if(empty($txt)){
-    $data = array("code"=>356,"message"=>"内容不能为空");
+if (empty($txt)) {
+    $data = array("code" => 356, "message" => "内容不能为空");
     echo json_encode($data);
     exit;
 }
 $time = date("Y-m-d H:i:s");
-$query=mysqli_query($link,"insert into comments(user,comment,addtime)values('$user','$txt','$time')");
-if($query)  {
-    $data = array("code" => 1, "message"=>"success","user" => $user , "txt" => $txt);
+$query = mysqli_query($link, "insert into comments(user,comment,addtime)values('$user','$txt','$time')");
+if ($query) {
+    $data = array("code" => 1, "message" => "success", "user" => $user, "txt" => $txt);
     echo json_encode($data);
 }
 
